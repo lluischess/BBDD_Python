00d37843ac4d063e894eacdcfca8f7bec5295c10
Lluis Casamajor <lluischess@gmail.com>
2021-03-04 00:14:56 +0100

    Instalar paquetes postgres

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-# Something is happening
+"""
+ Python and PostgresSQL
+
+"""
+# 1. Instalar paquetes para trabajar con postgress y mysql
+# 2. 
+
+
+
+#-----------------------------------------------------------
+# 1. Instalar paquetes para trabajar con postgress y mysql
+
+# Primero actualizamos pip en cmd: python -m pip install --upgrade pip --no-warn-script-location
+# Segundo instalamos el paquete postgress: pip install psycopg2
+# O el de Mysql: pip install mysql-connector
+
+#-----------------------------------------------------------

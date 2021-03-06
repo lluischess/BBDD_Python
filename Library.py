abc0a586ea5fd0cd96258090273c63d53e77415e
Lluis Casamajor <lluischess@gmail.com>
2021-03-06 11:40:55 +0100

    BBDD con tuplas

--- Library.py
+++ Library.py
@@ -4,6 +4,7 @@
 """
 # 1. Instalar paquetes para trabajar con postgress y mysql
 # 2. Conexion a Postgress
+# 3. Conexion a Postgress con clase
 
 
 # -----------------------------------------------------------
@@ -30,15 +31,32 @@ conexion_test_db = psycopg2.connect(user="postgres",
 cursor_test_db = conexion_test_db.cursor()
 
 # Asignamos sentencia SQL a una variable
-sql_select = "SELECT * FROM persona ORDER BY nombre"
+#sql_select = "SELECT * FROM persona WHERE id_persona = %s" 1 valor
+sql_select = "SELECT * FROM persona WHERE id_persona IN %s" # varios valores
 
-# Ejecutar sentencia
-cursor_test_db.execute(sql_select)
+entrada = input("Proporciona las Ids: (separado por comas)")
+tupla = tuple(entrada.split(","))
+#id_persona = input("Añade el Id de la persona:")
+tupla_ids = ((tupla),) #tupla de tuplas
+#llave_primeria = (id_persona,)#tuppla
+
+# Ejecutar sentenc1ia
+cursor_test_db.execute(sql_select,tupla_ids) # segundo parametro de sustitución de la id_persona
 
 # fetchall devuelve el resultado 
-resultado = cursor_test_db.fetchall()
-print(resultado)
+resultado = cursor_test_db.fetchall() #devuelve todos los registros
+for resul in resultado:
+    print(resul)
+
+# fetchone devuelve 1 registro
+#resultado2 = cursor_test_db.fetchone()
+#print(resultado2)
 
 # Cerrar conexion y cursor
 cursor_test_db.close()
 conexion_test_db.close()
+
+#--------------------------------------------------------------------------------
+# 3. Conexion a Postgress con clase
+
+
